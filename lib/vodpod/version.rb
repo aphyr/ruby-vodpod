10c08ea3f3d0d4662deb3979793a82d744c593cc
Kyle Kingsbury <kyle@vodpod.com>
2009-10-21 13:50:09 -0700

    Few more readme changes.

--- lib/vodpod/version.rb
+++ lib/vodpod/version.rb
@@ -2,7 +2,7 @@ module Vodpod
   APP_NAME = 'Vodpod'
   APP_VERSION = '2.0.0'
   APP_AUTHOR = 'Kyle Kingsbury'
-  APP_EMAIL = 'aphyr@aphyr.com'
-  APP_URL = 'http://aphyr.com/projects/ruby-vodpod'
+  APP_EMAIL = 'support@vodpod.com'
+  APP_URL = 'http://aphyr.com/projects/show/ruby-vodpod-bindings'
   APP_COPYRIGHT = 'Copyright (c) 2009 Kyle Kingsbury <aphyr@aphyr.com>. All rights reserved.'
 end
